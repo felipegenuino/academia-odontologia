d40ea5735950f0fdfc315276c5b7b8c1b6709f68
Felipe Genuino <design@felipegenuino.com>
2021-01-24 23:16:34 -0300

    update style header scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
-console.log('rodou')
+console.log('rodou');
+
+
+
+
+
+/* vanilla scroll
+-- Verifica se o header está na posição 0 caso não esteja adiciona background;
+*/
+
+let scrollpos = window.scrollY
+const header = document.querySelector("header.navbar")
+const header_height = header.offsetHeight
+
+const add_class_on_scroll = () => header.classList.add("bg-dark")
+const remove_class_on_scroll = () => header.classList.remove("bg-dark")
+
+window.addEventListener('scroll', function () {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+    // console.log(scrollpos);
+});
+
+// end vanilla scrol
