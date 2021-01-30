b4242c6d0f8a1a1f338697cf86571513506f8287
Felipe Genuino <design@felipegenuino.com>
2021-01-29 21:48:49 -0300

    update offcanvas draft mobile menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,10 +31,25 @@ window.addEventListener('scroll', function () {
 
 
 
+/*
+offcanvas
+========================= */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("app-mobile-sidebar").style.width = "250px";
+    document.getElementById("app-content").style.marginRight = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("app-mobile-sidebar").style.width = "0";
+    document.getElementById("app-content").style.marginRight = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
 /* ---------------------------- */
-
-
-
 
 
 
@@ -45,4 +60,7 @@ Animate On Scroll Library
 AOS.init();
 
 /* ---------------------------- */
+
+
+
 
