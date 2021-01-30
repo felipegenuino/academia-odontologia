bfb3bd1d7c1ca9f97f0fa932360d210154283dfc
Felipe Genuino <design@felipegenuino.com>
2021-01-30 11:01:59 -0300

    Hover list invist

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,14 +38,16 @@ offcanvas
 function openNav() {
     document.getElementById("app-mobile-sidebar").style.width = "250px";
     document.getElementById("app-content").style.marginRight = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("app-content-overlayer").classList.add('good');;
 }
+
+
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     document.getElementById("app-mobile-sidebar").style.width = "0";
     document.getElementById("app-content").style.marginRight = "0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("app-content-overlayer").classList.remove('good');
 }
 
 
